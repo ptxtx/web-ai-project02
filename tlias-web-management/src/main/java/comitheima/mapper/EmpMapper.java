682b626dce0042aadd85f28767e786f4b804edb8
ptx <3351626369@qq.com>
2026-05-18 09:31:14 +0800

    员工管理-查询（PageHelper)

--- tlias-web-management/src/main/java/comitheima/mapper/EmpMapper.java
+++ tlias-web-management/src/main/java/comitheima/mapper/EmpMapper.java
@@ -9,10 +9,16 @@ import java.util.List;
 @Mapper
 public interface EmpMapper {
 
-    @Select("SELECT COUNT(*) FROM emp")
-    public Long count();
-    @Select("select e.*,d.name deptName from emp e left join dept d on e.dept_id = d.id " +
-            "order by e.update_time desc limit #{start},#{pageSize};")//Emp中不能封装d.name  所以得增加属性
-    public List<Emp> list(Integer start, Integer pageSize);
+    //------------------------原始分页查询实现-----------
+//    @Select("SELECT COUNT(*) FROM emp")
+//    public Long count();
+//    @Select("select e.*,d.name deptName from emp e left join dept d on e.dept_id = d.id " +
+//            "order by e.update_time desc limit #{start},#{pageSize};")//Emp中不能封装d.name  所以得增加属性
+//    public List<Emp> list(Integer start, Integer pageSize);
+
+        @Select("select e.*,d.name deptName from emp e left join dept d on e.dept_id = d.id " +
+            "order by e.update_time desc")//Emp中不能封装d.name  所以得增加属性
+    public List<Emp> list();
+
 
 }
--- tlias-web-management/src/main/java/comitheima/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/comitheima/service/impl/EmpServiceImpl.java
@@ -1,5 +1,7 @@
 package comitheima.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import comitheima.mapper.EmpMapper;
 import comitheima.pojo.Emp;
 import comitheima.pojo.PageResult;
@@ -13,13 +15,24 @@ import java.util.List;
 public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpMapper empMapper;
+    //--------------------原始分页查询
+//    @Override
+//    public PageResult<Emp> page(Integer page, Integer pageSize){
+//        Long total = empMapper.count();
+//
+//        Integer start = (page-1)*pageSize;
+//        List<Emp> rows = empMapper.list(start, pageSize);
+//        return new PageResult<Emp>(total,rows);
+//    }
     @Override
     public PageResult<Emp> page(Integer page, Integer pageSize){
-        Long total = empMapper.count();
+        //1.设置分页参数
+        PageHelper.startPage(page,pageSize);
+        //2.执行查询
+        List<Emp> empList = empMapper.list();
 
-        Integer start = (page-1)*pageSize;
-        List<Emp> rows = empMapper.list(start, pageSize);
-        return new PageResult<Emp>(total,rows);
+        Page<Emp> p=(Page<Emp>) empList;
+        return new PageResult<Emp>(p.getTotal(),p.getResult());
     }
 
 }
